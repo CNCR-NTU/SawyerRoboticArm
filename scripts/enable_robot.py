3c4ca07787ae53ed5e357ea49e098b7e589cf644
QuenOlivier <quentin.olivier1996@gmail.com>
2018-07-26 13:44:06 +0100

    Change organisation

---
+++ scripts/enable_robot.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013-2018, Rethink Robotics Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import os
+import sys
+
+import rospy
+
+import intera_interface
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--state', const='state',
+                        dest='actions', action='append_const',
+                        help='Print current robot state')
+    parser.add_argument('-e', '--enable', const='enable',
+                        dest='actions', action='append_const',
+                        help='Enable the robot')
+    parser.add_argument('-d', '--disable', const='disable',
+                        dest='actions', action='append_const',
+                        help='Disable the robot')
+    parser.add_argument('-r', '--reset', const='reset',
+                        dest='actions', action='append_const',
+                        help='Reset the robot')
+    parser.add_argument('-S', '--stop', const='stop',
+                        dest='actions', action='append_const',
+                        help='Stop the robot')
+    args = parser.parse_args(rospy.myargv()[1:])
+
+    if args.actions == None:
+        parser.print_usage()
+        parser.exit(0, "No action defined")
+
+    rospy.init_node('sdk_robot_enable')
+    rs = intera_interface.RobotEnable(intera_interface.CHECK_VERSION)
+
+    try:
+        for act in args.actions:
+            if act == 'state':
+                print rs.state()
+            elif act == 'enable':
+                rs.enable()
+            elif act == 'disable':
+                rs.disable()
+            elif act == 'reset':
+                rs.reset()
+            elif act == 'stop':
+                rs.stop()
+    except Exception, e:
+        rospy.logerr(e.strerror)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
