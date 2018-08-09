a6a575d97a5cd718099e15e066b23b5570abb5ad
QuenOlivier <quentin.olivier1996@gmail.com>
2018-08-09 15:08:11 +0100

    Add script

---
+++ sawyer_robot_code/src/sawyer_robot_code/get_joint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rospy
+import intera_interface
+import sys, getopt
+import argparse
+
+## @file print_joint.py
+# 
+# \brief Script used to print the current joint positions of the robot.
+
+##Main function.
+#
+# \details Get the joint angles and return them.
+# \return tab of float containing the angle values of the robot
+def main():
+	angle=[0,0,0,0,0,0,0]
+	limb=intera_interface.Limb("right")
+	angles=limb.joint_angles()
+	for i in range(7):
+		angle[i]=angles['right_j'+`i`]
+	return angle
+
+if __name__ == '__main__':
+	try:
+		rospy.init_node('Get_position',anonymous=True)
+		angles=main()
+		print "\n"
+		print angles
+		print "\n"
+	except rospy.ROSInterruptException:
+		pass
