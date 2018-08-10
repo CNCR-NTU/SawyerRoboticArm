21af6f7eae580d6cf3634f8e5d7e799943baeeac
QuenOlivier <quentin.olivier1996@gmail.com>
2018-08-10 16:10:16 +0100

    Clean code

--- sawyer_robot_code/scripts/display_camera.py
+++ sawyer_robot_code/scripts/display_camera.py
@@ -57,62 +57,56 @@ def main():
         - exposure: [0-100], -1 for auto-exposure
         - gain: [0-79], -1 for auto-gain
     """
-    rp = intera_interface.RobotParams()
-    valid_cameras = rp.get_camera_names()
-    if not valid_cameras:
-        rp.log_message(("Cannot detect any camera_config"
+	rp = intera_interface.RobotParams()
+	valid_cameras = rp.get_camera_names()
+	if not valid_cameras:
+		rp.log_message(("Cannot detect any camera_config"
             " parameters on this robot. Exiting."), "ERROR")
-        return
-    arg_fmt = argparse.RawDescriptionHelpFormatter
-    parser = argparse.ArgumentParser(formatter_class=arg_fmt,
+		return
+	arg_fmt = argparse.RawDescriptionHelpFormatter
+	parser = argparse.ArgumentParser(formatter_class=arg_fmt,
                                      description=main.__doc__)
-    parser.add_argument(
-        '-c', '--camera', type=str, default="head_camera",
+	parser.add_argument('-c', '--camera', type=str, default="head_camera",
         choices=valid_cameras, help='Setup Camera Name for Camera Display')
-    parser.add_argument(
-        '-r', '--raw', action='store_true',
+	parser.add_argument('-r', '--raw', action='store_true',
         help='Specify use of the raw image (unrectified) topic')
-    parser.add_argument(
-        '-e', '--edge', action='store_true',
+	parser.add_argument('-e', '--edge', action='store_true',
         help='Streaming the Canny edge detection image')
-    parser.add_argument(
-        '-g', '--gain', type=int,
+	parser.add_argument('-g', '--gain', type=int,
         help='Set gain for camera (-1 = auto)')
-    parser.add_argument(
-        '-x', '--exposure', type=float,
-        help='Set exposure for camera (-1 = auto)')
-    args = parser.parse_args(rospy.myargv()[1:])
+	parser.add_argument('-x', '--exposure', type=float,help='Set exposure for camera (-1 = auto)')
+	args = parser.parse_args(rospy.myargv()[1:])
 
-    print("Initializing node... ")
-    rospy.init_node('camera_display', anonymous=True)
-    cameras = intera_interface.Cameras()
-    if not cameras.verify_camera_exists(args.camera):
-        rospy.logerr("Could not detect the specified camera, exiting the example.")
-        return
-    rospy.loginfo("Opening camera '{0}'...".format(args.camera))
-    cameras.start_streaming(args.camera)
-    rectify_image = not args.raw
-    use_canny_edge = args.edge
-    cameras.set_callback(args.camera, show_image_callback,
+	print("Initializing node... ")
+	rospy.init_node('camera_display', anonymous=True)
+	cameras = intera_interface.Cameras()
+	if not cameras.verify_camera_exists(args.camera):
+		rospy.logerr("Could not detect the specified camera, exiting the example.")
+		return
+	rospy.loginfo("Opening camera '{0}'...".format(args.camera))
+	cameras.start_streaming(args.camera)
+	rectify_image = not args.raw
+	use_canny_edge = args.edge
+	cameras.set_callback(args.camera, show_image_callback,
         rectify_image=rectify_image, callback_args=(use_canny_edge, args.camera))
 
     # optionally set gain and exposure parameters
-    if args.gain is not None:
-        if cameras.set_gain(args.camera, args.gain):
-            rospy.loginfo("Gain set to: {0}".format(cameras.get_gain(args.camera)))
+	if args.gain is not None:
+		if cameras.set_gain(args.camera, args.gain):
+			rospy.loginfo("Gain set to: {0}".format(cameras.get_gain(args.camera)))
 
-    if args.exposure is not None:
-        if cameras.set_exposure(args.camera, args.exposure):
-            rospy.loginfo("Exposure set to: {0}".format(cameras.get_exposure(args.camera)))
+	if args.exposure is not None:
+        	if cameras.set_exposure(args.camera, args.exposure):
+            		rospy.loginfo("Exposure set to: {0}".format(cameras.get_exposure(args.camera)))
 
-    def clean_shutdown():
-        print("Shutting down camera_display node.")
-        cv2.destroyAllWindows()
+	def clean_shutdown():
+		print("Shutting down camera_display node.")
+		cv2.destroyAllWindows()
 
-    rospy.on_shutdown(clean_shutdown)
-    rospy.loginfo("Camera_display node running. Ctrl-c to quit")
-    rospy.spin()
+	rospy.on_shutdown(clean_shutdown)
+	rospy.loginfo("Camera_display node running. Ctrl-c to quit")
+	rospy.spin()
 
 
 if __name__ == '__main__':
-    main()
+	main()
